702902ba774ccc3e40871235028f378e420bee6a
Donghyeok <tjehdgur1500@naver.com>
2020-06-16 23:56:55 +0900

    shorts create

--- sortURL_api/sorten/models.py
+++ sortURL_api/sorten/models.py
@@ -5,4 +5,5 @@ from django.contrib.auth.models import User
 class Sorten(models.Model):
     owner = models.ForeignKey(User, related_name='urls', on_delete=models.CASCADE)
     selfurl = models.URLField(max_length=200)
+    shorturl = models.URLField(max_length=200)
     random_int = models.BigIntegerField()
--- sortURL_api/sorten/views.py
+++ sortURL_api/sorten/views.py
@@ -21,23 +21,28 @@ class SortenViewSet(ModelViewSet):
         else:
             return super().get_queryset()
 
+
     def perform_create(self, serializer):
+
+        a = Sorten.objects.last()
+        marge = a.id+1
+        marge = base62.encode(marge)
         serializer.save(
-            owner=self.request.user
+            owner=self.request.user,
+            shorturl=marge,
         )
-
     # url shorten
-    def create_short_url(self):
-
-        random_int = self.random_num_create()
-
-        Sorten.objects.get(random_int=random_int)
-
-        url_obj = Sorten(random_int=random_int)
-        url_obj.save()
-        return url_obj
-
-    def random_num_create(self):
-        num = self.randint(1, 218340105584895)
-        return num
+    # def create_short_url(self):
+    #
+    #     random_int = self.random_num_create()
+    #
+    #     Sorten.objects.get(random_int=random_int)
+    #
+    #     url_obj = Sorten(random_int=random_int)
+    #     url_obj.save()
+    #     return url_obj
+    #
+    # def random_num_create(self):
+    #     num = self.randint(1, 218340105584895)
+    #     return num
 
