ddf312745f2f279346abc31ccb457474fa4880b3
Donghyeok <tjehdgur1500@naver.com>
2020-06-18 12:46:12 +0900

    update: action method

--- sortURL_api/users/views.py
+++ sortURL_api/users/views.py
@@ -1,14 +1,13 @@
 from django.shortcuts import render
 from django.contrib.auth.models import User
 from rest_framework import status
+from rest_framework.authtoken.serializers import AuthTokenSerializer
 from rest_framework.decorators import action
-from rest_framework.views import APIView
 from rest_framework.viewsets import ModelViewSet
 
 from sorten.paginations import IdPagination
 from .serializer import UserSerializer
 from .permissions import IsOwner
-from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.authtoken.models import Token
 from rest_framework.response import Response
 
@@ -26,8 +25,8 @@ class UserViewSet(ModelViewSet):
 
     @action(methods=['POST'], detail=False)
     def login(self, request, *args, **kwargs):
-        serializer = self.serializer_class(data=request.data,
-                                           context={'request': request})
+        serializer = AuthTokenSerializer(data=request.data,
+                                         context={'request': request})
         serializer.is_valid(raise_exception=True)
         user = serializer.validated_data['user']
         token, created = Token.objects.get_or_create(user=user)
